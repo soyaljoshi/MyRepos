5f72a288a9e7608c282f4f5963e22945d447983a
Soyal Joshi <soyal@mavorion.comi>
2017-04-06 21:46:09 +0545

    package changes

--- resources/views/frontend/partials/packages.blade.php
+++ resources/views/frontend/partials/packages.blade.php
@@ -1,4 +1,4 @@
-<!-- <section class="block whitish">
+    <section class="block whitish">
         <div class="container">
             <div class="fixed whitish" style="background:url({{asset('assets/frontend/images/parallax1.jpg')}});"></div>
             <div class="sec-title">
@@ -21,7 +21,7 @@
                                       <img src="{{ asset('assets/frontend/images/blank-image.jpg') }}" alt="" />
                                 @endif
                                     
-                                    <h4><span>{{$package->title}}</span></h4>
+                                    <h4><span>{{$package->title}} | $key</span></h4>
                                 </div>
                             </div>
                             @if ($key%4 ==0)
@@ -38,38 +38,4 @@
                 </ul>
             </div>          
         </div>
-    </section> -->
-
-    <section class="block whitish">
-        <div class="container">
-            <div class="fixed whitish" style="background:url({{asset('assets/frontend/images/parallax1.jpg')}});"></div>
-            <div class="sec-title">
-                <h2><strong>Our</strong> Partners</h2>
-            </div>  
-            <div class="mission-carousel">
-                <ul class="slides">
-                    @foreach($packages as $key => $package)
-                    <li>                
-                        <div class="row">                 
-                            <div class="col-md-3">
-                                <div class="service">
-                                @if(count($images->getImage('Package',$package->id)) > 0 )
-                                    @foreach($image as $images)
-                                        @if($images->imageable_type === 'Package' && $images->imageable_id == $package->id )
-                                            <img class="home_popular" src="{{ asset($images->thumbnail(141,139)) }}" alt="">
-                                        @endif
-                                    @endforeach                                            
-                                @else 
-                                      <img src="{{ asset('assets/frontend/images/blank-image.jpg') }}" alt="" />
-                                @endif                          
-                                    <h4><span>{{$package->title}}</span></h4>
-                                </div>
-                            </div>                 
-                        </div>
-                    </li>  
-                    @endforeach
-                </ul>
-            </div>          
-        </div>
     </section>
-        
